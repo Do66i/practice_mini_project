33444e7d314837614c9d123ccd8a0c7eb5dbf29e
DOBBI <cocomong_94@naver.com>
2022-03-20 22:34:41 +0900

    20220320-REACT폐관수련

--- shop/src/component/detail.js
+++ shop/src/component/detail.js
@@ -1,1 +1,12 @@
 console.log("Hello World")
+
+function Card(props) {
+  return (
+    <div className='col-md-4'>
+      <img src={props.images} width="50%"></img>
+      <h4>{props.zzanggu.title}</h4>
+      <p>{props.zzanggu.content} <br></br>
+        귀여움수치 : {props.zzanggu.price}</p>
+    </div>
+  )
+}

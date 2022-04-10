9bbf179300d2072aace5de12882dae8a98e4764e
DOBBI <cocomong_94@naver.com>
2022-04-10 22:41:53 +0900

    setDiray

--- shop/src/main.jsx
+++ shop/src/main.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import { BrowserRouter } from 'react-router-dom'; // ./ 없으면 라이브러리라 생각해도 무관할듯
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom"; // ./ 없으면 라이브러리라 생각해도 무관할듯
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+
+  document.getElementById("root")
   // 대충 🐘 App이라는 컴포넌트를 HTML 파일에서 id가 root인 곳에다가 App을 갖다붙여주시오! 🐘 라는 뜻
-)
+);
